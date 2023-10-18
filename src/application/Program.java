0be3dfc00b4c7c1b0962e1303320ca3055e2fc9f
Victor Oleskovicz <victoroleskovicz1@gmail.com>
2023-10-18 19:34:49 -0300

    Project Created

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ public class Program {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("Puta que paraio");
+		System.out.println("Commit teste");
 	}
 
 }
